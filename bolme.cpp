20999c71d5b22fe4cfa256548409b575f9c858cb
MeryemKavak <kavakmeryem07@gmail.com>
2025-10-30 12:04:00 +0300

    Update bolme.cpp

--- bolme.cpp
+++ bolme.cpp
@@ -1,20 +1,4 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int sayi1, sayi2;
-
-    cout << "Birinci sayiyi gir: ";
-    cin >> sayi1;
-
-    cout << "Ikinci sayiyi gir: ";
-    cin >> sayi2;
-
-    if (sayi2 != 0) {
-        cout << "Bolum: " << sayi1 / sayi2 << endl;
-    } else {
-        cout << "Sayi sifira bolunemez!" << endl;
-    }
-
-    return 0;
+double bolme(double a, double b) {
+    if (b == 0) return 0;  // Sıfıra bölme kontrolü
+    return a / b;
 }
