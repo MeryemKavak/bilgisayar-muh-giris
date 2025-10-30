d0bc314fa79413a0c7b4b43542fed33f375a2bf1
MeryemKavak <kavakmeryem07@gmail.com>
2025-10-30 12:03:11 +0300

    Update toplama.cpp

--- toplama.cpp
+++ toplama.cpp
@@ -1,18 +1,3 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int sayi1, sayi2, toplam;
-
-    cout << "Birinci sayiyi gir: ";
-    cin >> sayi1;
-
-    cout << "Ikinci sayiyi gir: ";
-    cin >> sayi2;
-
-    toplam = sayi1 + sayi2;
-
-    cout << "Toplam: " << toplam << endl;
-
-    return 0;
+double toplama(double a, double b) {
+    return a + b;
 }
